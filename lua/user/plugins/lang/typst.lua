d8dec6b07c852fa78a5e4926b17849429f349f18
Trouble-Truffle <perigordtruffle7318@gmail.com>
2025-09-21 22:26:14 +0800

    feat: Add word processor functions to typst mode

--- lua/user/plugins/lang/typst.lua
+++ lua/user/plugins/lang/typst.lua
@@ -37,6 +37,30 @@ return {
               }, { bufnr = bufnr })
             end, { desc = "Pin as mainfile", noremap = true })
 
+            local surround = function(delimStart, delimEnd)
+              delimEnd = delimEnd or delimStart
+              return function()
+                local _, lstart, cstart, _ = unpack(vim.fn.getpos("v"))
+                local line1 = vim.fn.getline(lstart)
+                local new_line1 = line1:sub(1, cstart - 1) .. delimStart .. line1:sub(cstart)
+                vim.fn.setline(lstart, new_line1)
+
+                local _, lend, cend, _ = unpack(vim.fn.getpos("."))
+
+                if lstart == lend then
+                  cend = cend + string.len(delimStart)
+                end
+                local line2 = vim.fn.getline(lend)
+                local new_line2 = line2:sub(1, cend) .. delimEnd .. line2:sub(cend + 1)
+                vim.fn.setline(lend, new_line2)
+              end
+            end
+
+            vim.keymap.set("v", "<C-b>", surround("*"), { expr = false })
+            vim.keymap.set("v", "<C-i>", surround("_"), { expr = false })
+            vim.keymap.set("v", "<C-`>", surround("`"), { expr = false })
+            vim.keymap.set("v", "<C-u>", surround("#underline([", "])"), { expr = false })
+
 
             vim.keymap.set("n", "<leader>lW", function()
               local job = require("plenary.job")
